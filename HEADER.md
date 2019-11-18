2145c46f887fa956d536efcc2ac0652ae65d92ff
Robert Kaussow <mail@geeklabor.de>
2019-11-18 14:49:26 +0100

    initial commit

---
+++ HEADER.md
@@ -0,0 +1,4 @@
+# apache
+
+[![Build Status](https://drone.owncloud.com/api/badges/owncloud-ansible/apache/status.svg)](https://drone.owncloud.com/owncloud-ansible/apache)
+
---
+++ molecule/centos7/tests/test_default.py
@@ -0,0 +1,14 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']
+).get_hosts('all')
+
+
+def test_apache_running_and_enabled(host):
+    apache = host.service("httpd24-httpd")
+
+    assert apache.is_running
+    assert apache.is_enabled
