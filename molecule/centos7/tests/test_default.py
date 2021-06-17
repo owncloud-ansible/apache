6d444be802040e5f97c70bedb9778a804ea4581d
Robert Kaussow <mail@thegeeklab.de>
2021-06-17 16:49:50 +0200

    fix: fix unhandled ssl port config

--- molecule/centos7/tests/test_default.py
+++ molecule/centos7/tests/test_default.py
@@ -12,3 +12,8 @@ def test_apache_running_and_enabled(host):
 
     assert apache.is_running
     assert apache.is_enabled
+
+
+def test_apache_network(host):
+    assert host.socket("tcp://127.0.0.1:8080").is_listening
+    assert host.socket("tcp://127.0.0.1:8443").is_listening
